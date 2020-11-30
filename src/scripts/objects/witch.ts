3a0742c5c83d757b453c197e50800cadce06d3b0
maxkrieger <themaxaxis@gmail.com>
2020-11-30 11:59:36 -0500

    Use arrow keys instead

--- src/scripts/objects/witch.ts
+++ src/scripts/objects/witch.ts
@@ -3,7 +3,6 @@ import { Physics } from "phaser";
 export default class Witch extends Phaser.Physics.Arcade.Sprite {
   isMe: boolean;
   id: string;
-  destination: { worldX: number; worldY: number };
   constructor(
     scene: Phaser.Scene,
     x: number,
@@ -12,7 +11,6 @@ export default class Witch extends Phaser.Physics.Arcade.Sprite {
     isMe: boolean
   ) {
     super(scene, x, y, "witch");
-    this.destination = { worldX: x, worldY: y };
     this.isMe = isMe;
     this.id = id;
     scene.add.existing(this);
@@ -20,18 +18,6 @@ export default class Witch extends Phaser.Physics.Arcade.Sprite {
     this.setCollideWorldBounds(true);
     this.setPipeline("Lighting2D");
   }
-  setDestination = (x, y: number) => {
-    this.destination = { worldX: x, worldY: y };
-  };
-  update = () => {
-    const targetDistance = Phaser.Math.Distance.Between(
-      this.x,
-      this.y,
-      this.destination.worldX,
-      this.destination.worldY
-    );
-    if (targetDistance < 4) {
-      this.body.reset(this.destination.worldX, this.destination.worldY);
-    }
-  };
+  setDestination = (x, y: number) => {};
+  update = () => {};
 }
--- src/scripts/scenes/mainScene.ts
+++ src/scripts/scenes/mainScene.ts
@@ -23,7 +23,6 @@ export default class MainScene extends Phaser.Scene {
     this.add.image(1376, 0, "bg").setOrigin(0).setPipeline("Light2D");
 
     this.cursor = this.input.keyboard.createCursorKeys();
-    this.input.addListener("pointerdown", this.onDown);
 
     this.witches["bla"] = new Witch(
       this,
@@ -43,12 +42,26 @@ export default class MainScene extends Phaser.Scene {
 
     this.lights.enable().setAmbientColor(0x808080);
     this.lights.addLight(200, 200, 100, 0xff0000, 8);
+
+    this.cursor.down?.on("down", this.setPlayerY(200));
+    this.cursor.down?.on("up", this.setPlayerY(0));
+    this.cursor.up?.on("down", this.setPlayerY(-200));
+    this.cursor.up?.on("up", this.setPlayerY(0));
+    this.cursor.left?.on("down", this.setPlayerX(-200));
+    this.cursor.left?.on("up", this.setPlayerX(0));
+    this.cursor.right?.on("down", this.setPlayerX(200));
+    this.cursor.right?.on("up", this.setPlayerX(0));
   }
 
-  onDown = () => {
-    const { worldX, worldY } = this.input.mousePointer;
-    this.witches["bla"].setDestination(worldX, worldY);
-    this.physics.moveTo(this.witches["bla"], worldX, worldY, 480);
+  setPlayerY = (v: number) => () => {
+    if (v !== 0 || (!this.cursor.down?.isDown && !this.cursor.up?.isDown)) {
+      this.witches["bla"].setVelocityY(v);
+    }
+  };
+  setPlayerX = (v: number) => () => {
+    if (v !== 0 || (!this.cursor.left?.isDown && !this.cursor.right?.isDown)) {
+      this.witches["bla"].setVelocityX(v);
+    }
   };
 
   update() {
